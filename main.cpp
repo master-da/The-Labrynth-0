409a428bb43264c1498c7de6607350d75e8db7d0
master-da <mdnokib2000@gmail.com>
2021-04-21 12:39:02 +0600

    creating base

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <stdlib.h>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <cstdio>
+#include <vector>
+#include <map>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#define error_sdl printf("sdl error. Error: %s\n", SDL_GetError());
+#define error_img printf("image error. Error: %s\n", IMG_GetError());
+#define error_tru printf("ttf error. Error: %s\n", TTF_GetError());
+
+const int WIDTH = 1280;
+const int HEIGHT = 720;
+
+
+int main(int argc, char* argv[])
+{
+    
+    
+    return 0;
+}
