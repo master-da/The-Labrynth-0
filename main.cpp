1211838a984a47ab3066953096024491fa0c329f
master-da <mdnokib2000@gmail.com>
2021-04-28 12:33:34 +0600

    more changes to button and sprite

--- main.cpp
+++ main.cpp
@@ -14,7 +14,6 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 
-#include "UI.cpp"
 
 #define error {printf("sdl error in line %d. Error: %s\n", __LINE__, SDL_GetError()); close();}
 #define error_i {printf("image error in line %d. Error: %s\n", __LINE__, IMG_GetError()); close();}
@@ -28,11 +27,6 @@ void close();
 SDL_Window* gWindow = NULL;
 SDL_Renderer* gRenderer = NULL;
 SDL_Rect VIEWPORT_GLOBAL = { 0, 0, WIDTH, HEIGHT };
-enum buttonStatus{
-    BUTTON_UNPRESSED,
-    BUTTON_HOVER,
-    BUTTON_PRESSED
-};
 
 void init(){
     if(SDL_Init( SDL_INIT_VIDEO ) < 0) error;
@@ -88,10 +82,10 @@ class LTexture{
 class Button{
     public:
     Button(){
-        int x = y = width = height = 0;
+        int x = y = given_width= my_width = given_height= my_height = 0;
         button = NULL;
         buttonStatus = BUTTON_UNPRESSED;
-        dest = {0, 0, width, height};
+        dest = {0, 0, given_width, given_height};
     }
 
     void loadFromImage( std::string path , Uint8 r, Uint8 g ,Uint8 b ){ //loads all button states as a texture
@@ -111,9 +105,9 @@ class Button{
             int xc, yc;
             SDL_GetMouseState(&xc, &yc);
             if(xc < dest.x) buttonStatus = BUTTON_UNPRESSED;
-            else if(xc > dest.x+width) buttonStatus = BUTTON_UNPRESSED;
+            else if(xc > dest.x+my_width) buttonStatus = BUTTON_UNPRESSED;
             else if(yc < dest.y) buttonStatus = BUTTON_UNPRESSED;
-            else if(yc > dest.y+height) buttonStatus = BUTTON_UNPRESSED;
+            else if(yc > dest.y+my_height) buttonStatus = BUTTON_UNPRESSED;
             else {
                 if(e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT) buttonStatus = BUTTON_PRESSED;
                 else buttonStatus = BUTTON_HOVER;
@@ -121,20 +115,26 @@ class Button{
         }
     }
 
-    void setSize( int w, int h ){ //must be called before setDest. Sets the width and height of button itself
-        width = w;
-        height = h;
+    void setSize( int w, int h ){ //must be called before setDest. Sets the width and height of button itself in source
+        given_width = my_width = w;
+        given_height = my_height = h;
     }
 
-    void setDest(int x, int y){ //sets the position the button is rendered. Must have width and height defined
-        if(!width || !height) printtf("Warning! Button dimensions not set.\n";)
-        dest = {x, y, width, height};
+    //sets the position the button is rendered. Also needs width and height declared, defaulted to given_width
+    void setDest( int x, int y, int width, int height ){
+        if(!given_width || !given_height) printf("Warning! Button dimensions not set.\n");
+
+        my_width = width;
+        my_height = height;
+
+        dest = {x, y, my_width, my_height};
     }
 
     void render(){      //checks if the button is pressed or not before rendering
-        SDL_Rect source = {0, 0, width, height} ;
-        if(buttonStatus == BUTTON_PRESSED) source.x = 2*width;
-        else if( buttonStatus == BUTTON_HOVER) source.x = width;
+
+        SDL_Rect source = {0, 0, given_width, given_height} ;
+        if(buttonStatus == BUTTON_PRESSED) source.x = 2*given_width;
+        else if( buttonStatus == BUTTON_HOVER) source.x = given_width;
 
         SDL_RenderCopy( gRenderer, button, &source, &dest );
     }
@@ -144,9 +144,22 @@ class Button{
     SDL_Rect dest;
     int x = 0;
     int y = 0;
-    int width = 0;
-    int height = 0;
+
+    enum buttonStatus{
+        BUTTON_UNPRESSED,
+        BUTTON_HOVER,
+        BUTTON_PRESSED
+    };
+
+    int given_width = 0;
+    int given_height = 0;
+
+    int my_width = 0;
+    int my_height = 0;
+
     int buttonStatus = BUTTON_UNPRESSED;
+
+    
 };
 
 class Sprite{
@@ -171,6 +184,7 @@ class Sprite{
     }
 
     //sets the width and heiht of each sprite state. Must be called before setDest
+    //also sets no of sprite in the sheet, not of sprite per column and no of sprite per row
     void setinfo(int w, int h, int states , int per_row, int per_col ){ 
         width = w;
         height = h;
@@ -179,45 +193,47 @@ class Sprite{
         sprites_per_col = per_col;
     }
 
-    void setDest( int x1, int y1, int x2, int y2 ){//sets the rectangle to render sprite in. setInfo muts've been called
-        dest1 = {x1, y1, width, height};
-        dest2 = {x2, y2, width, height}; //im being lazy and rendering same sprite in 2 positions. I should fix.
+    void setDest( int x, int y ){//sets the rectangle to render sprite in. setInfo muts've been called
+        dest = {x, y, width, height};
     }
 
     void render(){
         int frame = slow/4; //frame holds which sprite state to render. Slowing speed by a factor of 4
         if(frame >= sprite_states) frame = slow = 0;    //resetting sprite to cycle around
         source = { width * (frame%sprites_per_row),  height * (frame/sprites_per_row), width , height };//setting which frame to be rendered
-        SDL_RenderCopy( gRenderer, texture, &source, &dest1 );
-        SDL_RenderCopy( gRenderer, texture, &source, &dest2 );
+        SDL_RenderCopy( gRenderer, texture, &source, &dest );
         slow++;
     }
 
     private:
     SDL_Texture* texture;
     SDL_Rect source;
-    SDL_Rect dest1;
-    SDL_Rect dest2;
+    SDL_Rect dest;
     int width = 0;
     int height = 0;
     int sprite_states = 0;
     int sprites_per_row = 0;
-    int sprites_per_col = =0;
+    int sprites_per_col = 0;
     int slow = 0;
 };
 
 Button butarr[4];
-Sprite fire_sprite;
+Sprite fire_sprite_left;
+Sprite fire_sprite_right;
 
 void loadmedia(){
     for(int i=0; i<4; i++) {
         butarr[i].loadFromImage( "png/sprite_sheet/button_pressed_sprite_dark.png", 255, 255, 255);
         butarr[i].setSize(240, 120);
-        butarr[i].setDest( 520, 100 + i*120 );
+        butarr[i].setDest( 400, 100 + i*120 , 480, 120);
     }
-    fire_sprite.loadFromImage( "png/sprite_sheet/fire_sprite.png", 0, 0, 0 );
-    fire_sprite.setSize( 128, 128 );
-    fire_sprite.setDest( 240, 296, 912, 296);
+    fire_sprite_left.loadFromImage( "png/sprite_sheet/fire_sprite.png", 0, 0, 0 );
+    fire_sprite_left.setinfo( 128, 128 , 25, 5, 5 );
+    fire_sprite_left.setDest( 240, 296 );
+
+    fire_sprite_right.loadFromImage( "png/sprite_sheet/fire_sprite.png", 0, 0, 0 );
+    fire_sprite_right.setinfo( 128, 128 , 25, 5, 5 );
+    fire_sprite_right.setDest( 912, 296 );
 }
 
 int main(int argc, char* argv[])
@@ -228,6 +244,7 @@ int main(int argc, char* argv[])
     SDL_Event e;
 
     while(!quit){
+
         while( SDL_PollEvent( &e ) ){
             if(e.type == SDL_QUIT) quit = 1;
             else for(int i=0; i<4; i++) butarr[i].eventProcessor( e );
@@ -235,8 +252,11 @@ int main(int argc, char* argv[])
 
         SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 255);
         SDL_RenderClear( gRenderer );
-        for(int i=0; i<4; i++) butarr[i].render();
-        fire_sprite.render();
+
+        for(int i=0; i<4; i++)butarr[i].render(); 
+        fire_sprite_left.render();
+        fire_sprite_right.render();
+
         SDL_RenderPresent( gRenderer );
     }
     
